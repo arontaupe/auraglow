242df5ba154bdb400f2a3caa98a3387376a75cfb
arontaupe <aron@petau.net>
2023-02-04 13:20:51 +0100

    working mac reset, removed errors on dry runs

--- Assets/Scripts/DynamicBeam.cs
+++ Assets/Scripts/DynamicBeam.cs
@@ -8,31 +8,40 @@ public class DynamicBeam : MonoBehaviour
     private MLInput.Controller controller;
     private LineRenderer beamLine;
 
+
+#if PLATFORM_LUMIN
     // Start is called before the first frame update
     void Start()
     {
         controller = MLInput.GetController(MLInput.Hand.Left);
         beamLine = GetComponent<LineRenderer>();
     }
+#endif
 
+#if PLATFORM_LUMIN
     // Update is called once per frame
     void Update()
     {
-        transform.position = controller.Position;
-        transform.rotation = controller.Orientation;
+        if(controller != null){
+            transform.position = controller.Position;
+            transform.rotation = controller.Orientation;
 
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit))
-        {
-            beamLine.useWorldSpace = true;
-            beamLine.SetPosition(0, transform.position);
-            beamLine.SetPosition(1, hit.point);
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit))
+            {
+                beamLine.useWorldSpace = true;
+                beamLine.SetPosition(0, transform.position);
+                beamLine.SetPosition(1, hit.point);
+            }
+            else
+            {
+                beamLine.useWorldSpace = true;
+                beamLine.SetPosition(0, transform.position);
+                beamLine.SetPosition(1, transform.forward * 5);
+            }
         }
-        else
-        {
-            beamLine.useWorldSpace = true;
-            beamLine.SetPosition(0, transform.position);
-            beamLine.SetPosition(1, transform.forward * 5);
-        }
+       
     }
+#endif
+
 }
